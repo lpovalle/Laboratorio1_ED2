97ee1072d8d01becc70a3e0bc4f26ac2680e9060
Luis Ovalle <luispalp@hotmail.com>
2017-09-20 20:42:25 -0600

    Se termino la serie 2

--- Lab1_compresion/Lab1_compresion/Program.cs
+++ Lab1_compresion/Lab1_compresion/Program.cs
@@ -14,14 +14,13 @@ namespace Lab1_compresion
 
         static string path;
         static string linea;
-        static bool operacion = false;
         static char[] chars;
         static int charslenght = 0;
         static string resultado;
 
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Inicia el programa: \n e.g: -c -f" + "(entre comillas)ruta del archivo");
             VerificarComando(Console.ReadLine());
 
             Console.ReadKey();
@@ -31,55 +30,86 @@ namespace Lab1_compresion
         {
             linea = lineacompleta;
 
-            string cmd = linea.Substring(0, 2);
-            string[] lineacmd = linea.Split('"');
-
-            path = lineacmd[1];
-
-
-            if (cmd == "-c")
+            try
             {
 
-                BinaryReader brc = new BinaryReader(File.OpenRead(path));
-                StreamReader src = new StreamReader(File.OpenRead(path));
+                string cmd = linea.Substring(0, 2);
+                string[] lineacmd = linea.Split('"');
 
-                string archivo = src.ReadToEnd();
+                path = lineacmd[1];
 
-                charslenght = archivo.Length;
-                chars = brc.ReadChars(charslenght);
 
-                compression_methods.RLE_compression compresion = new compression_methods.RLE_compression();
+                if (cmd == "-c")
+                {
 
-                resultado = compresion.Encode(charslenght, chars);
+                    BinaryReader brc = new BinaryReader(File.OpenRead(path));
+                    StreamReader src = new StreamReader(File.OpenRead(path));
 
-                StreamWriter swc = new StreamWriter(path + ".rlex");
-                swc.WriteLine(resultado);
-                swc.Close();
+                    string archivo = src.ReadToEnd();
 
-                Console.Write(resultado);
+                    charslenght = archivo.Length;
+                    chars = brc.ReadChars(charslenght);
+
+                    compression_methods.RLE_compression compresion = new compression_methods.RLE_compression();
+
+                    resultado = compresion.Encode(charslenght, chars);
+
+                    StreamWriter swc = new StreamWriter(path + ".rlex");
+                    swc.WriteLine(resultado);
+                    swc.Close();
+
+                    Console.Write("Compresión realizada con éxito:");
+
+                    FileInfo bcfile = new FileInfo(path);
+                    FileInfo acfile = new FileInfo(path + ".rlex");
+
+
+                    int original = Convert.ToInt32(bcfile.Length);
+                    int nuevo = Convert.ToInt32(acfile.Length);
+
+                    double compratio = ((double)nuevo / (double)original);
+                    double compfactor = ((double)original / (double)nuevo);
+                    double savingp = (((double)original - (double)nuevo) / (double)original) * 100;
+
+                    Console.WriteLine("Tamaño original:         " + Convert.ToString(original));
+                    Console.WriteLine("Tamaño nuevo:            " + Convert.ToString(nuevo));
+                    Console.WriteLine("Ratio de compresión:     " + Convert.ToString(compratio));
+                    Console.WriteLine("Factor de compresión:    " + Convert.ToString(compfactor));
+                    Console.WriteLine("Porcentaje ahorrado:     " + Convert.ToString(savingp) + "%");
+
+                }
+                else if (cmd == "-d")
+                {
+
+                    StreamReader srd = new StreamReader(File.OpenRead(path));
+
+                    string archivo = srd.ReadToEnd();
+
+
+                    compression_methods.RLE_compression descompresion = new compression_methods.RLE_compression();
+
+                    Console.WriteLine(descompresion.Decode(archivo));
+
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese un comando válido");
+                    VerificarComando(Console.ReadLine());
+
+
+                }
 
             }
-            else if (cmd == "-d")
+            catch (Exception ex)
             {
-
-                StreamReader srd = new StreamReader(File.OpenRead(path));
-
-                string archivo = srd.ReadToEnd();
-
-
-                compression_methods.RLE_compression descompresion = new compression_methods.RLE_compression();
-
-                Console.WriteLine(descompresion.Decode(archivo));
-
-            }
-            else
-            {
-                Console.WriteLine("Ingrese un comando válido");
+                Console.WriteLine("Error: " + ex);
+                Console.WriteLine("Escriba el comando de nuevo: ");
                 VerificarComando(Console.ReadLine());
-            }
 
 
             }
 
         }
     }
+}
+
